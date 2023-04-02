e69b08cac9b3103200b47e3f3f6a87d7a40349ed
Ogg <ogg.developer@gmail.com>
2023-04-03 00:31:55 +0200

    Added ability to delete workouts from history

--- app/src/main/java/com/oggtechnologies/orkout/ui/WorkoutHistoryScreen.kt
+++ app/src/main/java/com/oggtechnologies/orkout/ui/WorkoutHistoryScreen.kt
@@ -1,17 +1,27 @@
 package com.oggtechnologies.orkout.ui
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.oggtechnologies.orkout.model.store.State
+import com.oggtechnologies.orkout.model.store.Workout
 import com.oggtechnologies.orkout.model.store.doNavigateBack
+import com.oggtechnologies.orkout.model.store.doRemoveWorkout
 import com.oggtechnologies.orkout.redux.Dispatch
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Composable
-fun WorkoutHistoryScreen(state: State, dispatch: Dispatch) {
+fun WorkoutHistoryScreen(state: State, dispatch: Dispatch) = ConfirmationHandler {
     BackHandler {
         dispatch(doNavigateBack())
     }
@@ -29,9 +39,70 @@ fun WorkoutHistoryScreen(state: State, dispatch: Dispatch) {
         content = {
             LazyColumn {
                 itemsWithDividers(state.workoutHistory) { workout ->
-                    Text(text = workout.toString())
+                    WorkoutRow(workout, remove = {
+                        showConfirmDialog("Are you sure you want to delete this workout?") {
+                            dispatch(doRemoveWorkout(workout.id))
+                        }
+                    })
                 }
             }
         }
     )
+}
+
+@Composable
+fun WorkoutRow(workout: Workout, remove: () -> Unit) {
+    var expanded: Boolean by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { expanded = !expanded }
+            .padding(16.dp)
+    ) {
+        val weekDay = workout.startTime.format("EEEE")
+        val time = workout.startTime.format("HH:mm")
+        val date = workout.startTime.format("yyyy-MM-dd")
+        val duration =
+            if (workout.endTime == null) "Ongoing" else formatDuration(workout.endTime - workout.startTime)
+        val nExercises = workout.exercises.size
+        val nSets = workout.exercises.sumOf { it.sets.size }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column {
+                Text(text = "$weekDay $duration", fontSize = 22.sp)
+                Text(text = "$date $time", fontSize = 12.sp)
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Column {
+                Text(text = "$nExercises exercises")
+                Text(text = "$nSets sets")
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            SimpleStringOverflowMenu {
+                "Delete" does remove
+            }
+        }
+
+        if (expanded) {
+            Text(
+                text = workout.toString(),
+                fontSize = 14.sp,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
+    }
+}
+
+private fun Long.format(f: String): String = SimpleDateFormat(f).format(Date(this))
+
+private fun formatDuration(durationMillis: Long): String {
+    val seconds = durationMillis / 1000
+    val minutes = seconds / 60
+    val hours = minutes / 60
+    if (hours == 0L) {
+        return "${minutes % 60}m"
+    }
+    return "${hours}h${minutes % 60}m"
 }
